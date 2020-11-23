f1e59850b7febbdb597716570bb9c4de81689f79
Вит ноут <grumdrig@mail.ru>
2020-11-23 23:54:47 +0300

    start

---
+++ js/main.js
@@ -0,0 +1,24 @@
+jQuery(document).ready(function($){    
+
+
+    // // faq
+    // $('[data-faq]').click(function(){
+    //   $(this).find('.faq__title').toggleClass('open');
+    //   $(this).find('p').slideToggle(300);
+    // })
+    // slide to id
+    //   $('[data-scroll]').on("click","a", function (event) {
+    //     event.preventDefault();
+    //     var id  = $(this).attr('href'),
+    //         top = $(id).offset().top;
+    //     $('body,html').animate({scrollTop: top}, 1500);
+    // });
+    // burger
+
+    // $('[data-burger]').click(function(){
+    //   $('html').toggleClass("open");
+    //   $(this).toggleClass("open");
+    //   $('[data-nav]').toggleClass("open");
+    //   $('body').toggleClass('open');
+    // });
+})
