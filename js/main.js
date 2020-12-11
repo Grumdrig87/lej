49440f7ff4ab4f61986c075b68131065725df95a
Grumdrig87 <70903272+Grumdrig87@users.noreply.github.com>
2020-12-11 17:39:50 +0300

    1

--- js/main.js
+++ js/main.js
@@ -8,11 +8,61 @@ jQuery(document).ready(function($){
         });
     }
 
+    //slider
+    
+    $('.rev__upr-arr').hover( function(){
+        $(this).each(function() {
+            $('.rev__upr-arr').toggleClass('transp');
+        });
+        $(this).removeClass('transp')
+    })
+
+
+
+    // 
+
+    var helpers = {
+        addZeros: function (n) {
+            return (n < 10) ? n : '' + n;
+        }
+    };
+    
+    function sliderInit() {
+      var $slider = $('[data-rev]');
+      $slider.each(function() {
+        var $sliderParent = $(this).parent();
+        if ($($slider).length > 0) {
+            $($slider).slick({
+                dots: false,
+                speed: 300,
+                infinite: false,
+                slidesToShow: 1,
+                prevArrow: '[data-prev]',
+                nextArrow: '[data-next]'
+            });
+        }
+    
+        if ($(this).find('.rev__card').length > 1) {
+          $(this).siblings('.rev__upr').show();
+        }
+    
+        $(this).on('afterChange', function(event, slick, currentSlide){
+          $sliderParent.find('.rev__upr [data-curr]').html('0' + helpers.addZeros(currentSlide + 1));
+        });
+    
+        var sliderItemsNum = $(this).find('.slick-slide').not('.slick-cloned').length;
+        $sliderParent.find('.rev__upr [data-all]').html('0' + helpers.addZeros(sliderItemsNum));
+    
+      });
+    };
+    
+    sliderInit();
+
     // // faq
-    // $('[data-faq]').click(function(){
-    //   $(this).find('.faq__title').toggleClass('open');
-    //   $(this).find('p').slideToggle(300);
-    // })
+    $('[data-faq]').click(function(){
+      $(this).find('.faq__title').toggleClass('open');
+      $(this).find('p').slideToggle(300);
+    })
     // slide to id
     //   $('[data-scroll]').on("click","a", function (event) {
     //     event.preventDefault();
